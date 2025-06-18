000933a90c835f4d7129aa18279e29296979b611
dawsonkaybwsc <159972020+dawsonkaybwsc@users.noreply.github.com>
2025-06-19 09:02:44 +1200

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -9,6 +9,25 @@ pygame.init()
 
 #setup display for game with height and width
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Pingpong")
+
+block_color = (255, 255, 255)
+block_x = 5
+block_y = 150
+block_width = 150
+block_height = 10
+
+block1_x = 5
+block1_y = 150
+block1_width = 150
+block1_height = 390
+
+block2_x = 5
+block2_y = 5
+block2_width = 200
+block2_height = 200
+
+
 
 def process_input():
     for event in pygame.event.get():
@@ -20,8 +39,11 @@ def update(dt):
     #this updates the games variables
     pass
 def render():
-    #this renders the game on screen
-    pass
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, block_color, (block_height, block_width, block_x, block_y))
+    pygame.draw.rect(screen, block_color, (block1_height, block1_width, block1_x, block1_y))
+    pygame.draw.rect(screen, block_color, (block2_height, block2_width, block2_x, block2_y))
+    pygame.display.flip()
 
 running = True
 last_frame_time = time.time()
@@ -35,5 +57,6 @@ while running:
     running = process_input() 
     update(dt)
     render()
+
 
 running = False
